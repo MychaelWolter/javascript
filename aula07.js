0c67817b306fb60b940c47161c5b6b3699e8f7b3
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2024-10-10 16:30:06 -0400

    aual07.js

---
+++ aula07.js
@@ -0,0 +1,9 @@
+console.log("e - and - &&");
+console.log("ou - or - ||");
+console.log("nao - not - !");
+
+console.log("and - setiver F é F, senao é V");
+console.log("or - se tiver V é V, sena é F");
+
+let n1,n2,n3,n4;
+n1 = 10;
