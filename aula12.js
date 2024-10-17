e24ed1e10797eaaa6582479b6a956c0444256b7a
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2024-10-17 13:23:13 -0400

    Update aula12.js

--- aula12.js
+++ aula12.js
@@ -20,3 +20,13 @@ const soma = (v1,v2,v3)=>{
 let valores = [1,5,4,9];
 
 console.log(soma(...valores));
+
+const objs1 = document.getElementsByTagName("div");
+const objs2 = [...document.getElementsByTagName("div")];
+
+objs2.forEach(element => {
+    element.innerHTML="javascript";
+});
+
+console.log(objs1);
+console.log(objs2);
