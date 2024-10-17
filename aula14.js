84bdda10a9e33fadc58395816495175b63d3963d
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2024-10-17 15:19:13 -0400

    Create aula14.js

---
+++ aula14.js
@@ -0,0 +1,19 @@
+let colocacao = 5;
+
+switch(colocacao){
+    case 1:
+        console.log("1° lugar");
+        break;
+    case 2:
+        console.log("2° lugar");
+        break;
+    case 3:
+        console.log("3° lugar");
+        break;
+    case 4: case 5: case 6:
+        console.log("quase hein");
+        break;
+    default:
+        console.log("nem pra podio tu presta");
+        break;
+}
