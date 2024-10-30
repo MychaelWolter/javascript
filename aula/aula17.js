d4ecc530137a172bfb1f2e5ec4e1fd3f4a14f3c1
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2024-10-30 15:31:18 -0400

    Create aula17.js

---
+++ aula/aula17.js
@@ -0,0 +1,14 @@
+let n = 0;
+while(n < 10){
+    console.log(n);
+    n++;
+};
+
+let fatorial = 5;
+let total = 1;
+while(fatorial >= 1){
+    total*=fatorial;
+    fatorial--;
+};
+console.log("");
+console.log("fatorial de 5 é: " + total);//120
