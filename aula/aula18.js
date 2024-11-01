ea746bdefa3a52694802be7c649e9c7908572e0d
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2024-11-01 14:17:30 -0400

    Create aula18.js

---
+++ aula/aula18.js
@@ -0,0 +1,15 @@
+let n = 0;
+do{
+    console.log(n);
+    n++;
+}while(n<10);
+console.log("");
+
+let fat = 5;
+let soma = 1;
+do{
+    soma*=fat;
+    fat--;
+}while(fat>1);
+console.log(soma);
+console.log("");
