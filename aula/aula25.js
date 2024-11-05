40d125ee6677c769ada7e910f3b5967da1e76537
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2024-11-05 16:55:19 -0400

    Create aula25.js

---
+++ aula/aula25.js
@@ -0,0 +1,16 @@
+const soma = (v1,v2)=>{
+    let tot = v1+v2;
+    return tot;
+};
+
+console.log(soma(5,3));
+
+const nome = n=> {
+    return n;
+}
+
+console.log(nome("mychael"));
+
+const add = n => n+10;
+
+console.log(add(45));
