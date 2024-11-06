479c3498267cc5ed3ddebcb7bb775e91d57d3e18
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2024-11-06 16:44:16 -0400

    Create aula27.js

---
+++ aula/aula27.js
@@ -0,0 +1,38 @@
+console.log("colocar o * na function")
+function* cores(){
+    yield 'verde';
+    yield 'azul';
+};
+
+const itc = cores();
+console.log(itc.next().value);
+console.log(itc.next().value);
+console.log(itc.next().value);
+console.log("");
+
+function* perguntas(){
+    const nome = yield 'qual seu nome?';
+    const esporte = yield 'qual seu esporte favorito?';
+    return "seu nome é " + nome + ", seu esporte é " + esporte;
+};
+
+const itp = perguntas();
+console.log(itp.next().value);
+console.log(itp.next('mychael').value);
+console.log(itp.next('fut').value);
+console.log("");
+
+function* contador(){
+    let i = 0;
+    while(true){
+        yield i++;
+        if(i>9){
+            break;
+        };
+    };
+};
+
+const itcont = contador();
+for(let c of itcont){
+    console.log(c);
+};
