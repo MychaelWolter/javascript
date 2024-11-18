a7d85d52b6b1a60e5ed484428c46b6046086943a
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2024-11-18 14:57:21 -0400

    Update ex01.js

---
+++ exercicios/ex01.js
@@ -0,0 +1,18 @@
+alert("Boas vindas ao nosso site!");
+
+let nome = lua;
+let idade = 5;
+let numeroDeVendas = 50;
+let saldDisponvel = 1000;
+
+alert("Erro! Preencha todos os campos");
+
+const mensagemDeErro = "Erro! Preencha todos os campos";
+alert(mensagemDeErro);
+
+nome = prompt("Qual seu nome?");
+idade = prompt("Qual a sua idade?");
+
+if(idade >= 18){
+    alert("Pode tirar a habilitação!");
+};
