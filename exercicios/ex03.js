6f50b35b2d36dd7137827c4cd61216cd3dbad2c3
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2024-11-18 16:16:08 -0400

    Create ex03.js

---
+++ exercicios/ex03.js
@@ -0,0 +1,24 @@
+let c = 1;
+while(c<=10){
+    console.log(c);
+    c++;
+};
+
+let cont = 10;
+while(cont>=0){
+    console.log(cont);
+    cont--;
+};
+
+let contador = prompt("digite um numero?");
+while(contador>=0){
+    console.log(contador);
+    contador--;
+};
+
+let co = 0;
+let num = prompt("digite um numero?");
+while(co>=num){
+    console.log(co);
+    co++;
+};
