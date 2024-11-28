a38e5d3877572fe8658e8c8591d9e9d181dbc942
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2024-11-28 14:33:17 -0400

    Update aula29.js

--- aula/funcoes/aula29.js
+++ aula/funcoes/aula29.js
@@ -2,12 +2,21 @@ function aluno(nome, nota) {
     this.nome = nome;
     this.nota = nota;
 
-    this.dados_anonimo = function() {
-        setTimeout( function() {
+    this.dadosAnonimo = function() {
+        setTimeout(function() {
             console.log(this.nome);
             console.log(this.nota);
-        }, 2000);
+        }, 5000);
+    };
+
+    this.dadosArrow = function() {
+        setTimeout(() => {
+            console.log(this.nome);
+            console.log(this.nota);
+        }, 5000);
     };
 };
 
-aluno("Mychael", 10.0);
+const aluno1 = new aluno("Mychael", 10.0);
+aluno1.dadosAnonimo();
+aluno1.dadosArrow();
