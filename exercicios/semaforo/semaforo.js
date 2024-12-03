786db423207fee0fa78ff91c3d08c97ad2ae703a
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2024-12-03 16:28:56 -0400

    Update semaforo.js

--- exercicios/semaforo/semaforo.js
+++ exercicios/semaforo/semaforo.js
@@ -1,19 +1,31 @@
 const semaforo = document.getElementById("semaforo");
 const buttons = document.getElementById("buttons");
+let colorIndex = 0;
 
 const trafficLight = (event) => {
     turnOn[event.target.id]();
 };
 
-const changeColor = () => {
+const nextIndex = () => {
+    if(colorIndex < 2) {
+        colorIndex++;
+    }else {
+        colorIndex = 0;
+    };
+};
 
+const changeColor = () => {
+    const colors = ["red", "green", "yellow"];
+    const color = colors[colorIndex];
+    turnOn[color]();
+    nextIndex();
 };
 
 const turnOn = {
     "red": () => semaforo.src = "image/vermelho.png",
     "yellow": () => semaforo.src = "image/amarelo.png",
     "green": () => semaforo.src = "image/verde.png",
-    "automatic": () => setTimeout(changeColor, 1000)
+    "automatic": () => setInterval(changeColor, 1000)
 };
 
 buttons.addEventListener("click", trafficLight);
