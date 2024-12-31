8a89614a9ff06c52bebb6e77344fc33c1d9ac738
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2024-12-31 15:57:57 -0400

    Create ex05.js

---
+++ exercicios/ex05.js
@@ -0,0 +1,34 @@
+const firstName = "Mychael";
+const lastName = "Wolter";
+console.log(`O meu nome é ${firstName.toUpperCase} ${lastName.toLowerCase}`);
+
+const listName = "Mychael,Lucio,Wolter";
+console.log(listName.split(","));
+
+// string
+
+const number = 5;
+console.log(5 * 10);
+
+const numberString = 10;
+console.log(numberString.toString);
+
+// number
+
+console.log(3 == 3);
+console.log(3 === "3");
+
+// boolean
+
+const x = null;
+const y = undefined;
+
+// null and undefined
+
+const array = [1, 2, 3];
+const person = {
+    name : "mychael",
+    age : "20"
+};
+
+// arrays and objects
