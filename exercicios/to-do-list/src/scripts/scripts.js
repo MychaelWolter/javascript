c776d63e9ba6c6b3a178912f8c6e78aef1443acb
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2025-01-08 16:03:23 -0400

    update

---
+++ exercicios/to-do-list/src/scripts/scripts.js
@@ -0,0 +1,56 @@
+const createForm = document.querySelector("#create-form");
+const createInput = document.querySelector("#create-input");
+
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelButton = document.querySelector("#cancel-btn");
+
+const toDoList = document.querySelector("#to-do-list");
+
+// funções 
+
+const saveToDoList = (textCreateInput) => {
+
+    const itemToDoList = document.createElement("div");
+    itemToDoList.classList.add("item-to-do-list");
+
+    const textToDoList = document.createElement("p");
+    textToDoList.innerText = textCreateInput;
+    itemToDoList.appendChild(textToDoList);
+
+    const finishButton = document.createElement("button");
+    finishButton.classList.add("finish-to-do-list");
+    finishButton.innerHTML += `<i class="fa-solid fa-check"></i>`;
+    itemToDoList.appendChild(finishButton);
+
+    const editButton = document.createElement("button");
+    editButton.classList.add("edit-to-do-list");
+    editButton.innerHTML += `<i class="fa-solid fa-pen"></i>`;
+    itemToDoList.appendChild(editButton);
+
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("remove-to-do-list");
+    removeButton.innerHTML += `<i class="fa-solid fa-xmark"></i>`;
+    itemToDoList.appendChild(removeButton);
+
+    toDoList.appendChild(itemToDoList);
+    console.log(itemToDoList);
+    console.log(toDoList);
+    
+};
+
+
+// eventos
+createForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const createInputValue = createInput.value;
+    
+    createInputValue ? saveToDoList(createInputValue) : console.log("Escreva alguma tarefa");
+});
+
+editForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const editInputValue = editInput.value;
+    
+
+});
