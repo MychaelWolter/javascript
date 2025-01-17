da608f7b234f39fc58bc6c09324e97744920f307
Mychael Lucio Wolter dos Santos <157067201+MychaelWolter@users.noreply.github.com>
2025-01-17 13:27:43 -0400

    update aula 35.js

--- aula/dom/aula35/aula35.js
+++ aula/dom/aula35/aula35.js
@@ -2,16 +2,18 @@ const left = document.querySelector("#left");
 const right = document.querySelector("#right");
 const buttonCopy = document.querySelector("#buttonCopy");
 const allItens = [...document.querySelectorAll(".item")];
+const select = [...document.querySelectorAll(".select")];
 
 allItens.map((element) => {
     element.addEventListener("click", (event) => {
-        const item = event.target;
-        item.classList.toggle("select");
+        const itemClicked = event.target;
+        itemClicked.classList.toggle("select");
     });
 });
 
-buttonCopy.addEventListener("click", () => {
+buttonCopy.addEventListener("click" , () => {
     const itemSelect = [...document.querySelectorAll(".select")];
+
     itemSelect.map((element) => {
         right.appendChild(element);
     });
