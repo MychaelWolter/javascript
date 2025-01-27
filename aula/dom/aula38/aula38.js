f402d59004d3a90188240f2dee42cd8d70562f50
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2025-01-27 16:36:55 -0400

    Update aula38.js

--- aula/dom/aula38/aula38.js
+++ aula/dom/aula38/aula38.js
@@ -11,6 +11,8 @@ console.log(allItem[5].previousElementSibling);
 console.log(document.getRootNode());
 console.log(allItem[0].ownerDocument);
 
+
+
 const childrenleft = [...left.children];
 console.log(childrenleft);
 
@@ -19,4 +21,19 @@ childrenleft.shift();
 
 childrenleft.map((elementLeft) => {
     left.appendChild(elementLeft);
-}); 
+});
+
+
+
+console.log(left.hasChildNodes());
+console.log(allItem[4].hasChildNodes());
+console.log(allItem[4].childNodes);
+console.log(allItem[4].children);
+
+left.children.length > 0 ? console.log(`left possui ${left.children.length} fihos`) : console.log(`left não possui filhos`) ;
+
+allItem[0].children.length > 0 ? console.log(`item 5 possui filhos`) : console.log(`item 5 não possui`);
+
+allItem.map((item) => {
+    console.log(item.hasChildNodes());
+});
