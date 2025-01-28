a9ead56c89f55e2ef7f58f47e7283034e4726d21
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2025-01-28 14:02:08 -0400

    aula 41

---
+++ aula/dom/aula41/aula41.js
@@ -0,0 +1,20 @@
+const containerInput = document.querySelector("#container-input");
+const inputTecnologia = document.querySelector("#input-teconologia");
+const button = document.querySelector("button");
+
+const container = document.querySelector("#container");
+
+
+
+const createItem = (itemValue) => {
+    const div
+};
+
+
+
+button.addEventListener("click", (event) => {
+    
+    event.preventDefault();
+    const inputValue = inputTecnologia.value;
+    createItem(inputValue);
+});
