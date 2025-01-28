f2a6889ad2f3b76f672c677d7c9457aee9c0f141
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2025-01-28 16:02:45 -0400

    aula 41 js

--- aula/dom/aula41/aula41.js
+++ aula/dom/aula41/aula41.js
@@ -1,13 +1,31 @@
 const containerInput = document.querySelector("#container-input");
 const inputTecnologia = document.querySelector("#input-teconologia");
 const button = document.querySelector("button");
-
 const container = document.querySelector("#container");
 
 
 
 const createItem = (itemValue) => {
-    const div
+
+    const div = document.createElement("div");
+    div.setAttribute("class", "programação");
+    div.setAttribute("id", itemValue);
+    div.innerText = itemValue;
+    container.appendChild(div);
+};
+
+
+
+const removeItem = (itemRemove) => {
+    const allItem = [...document.querySelectorAll(".programação")];
+
+    allItem.forEach((item) => {
+        if (item.id === itemRemove){
+            item.removeAttribute("id");
+            item.removeAttribute("class");
+            item.remove();
+        };
+    });
 };
 
 
@@ -17,4 +35,14 @@ button.addEventListener("click", (event) => {
     event.preventDefault();
     const inputValue = inputTecnologia.value;
     createItem(inputValue);
+});
+
+
+
+container.addEventListener("click", (event) => {
+    const target = event.target.id;
+
+    if (target !== "container"){
+        removeItem(target);
+    };
 });
