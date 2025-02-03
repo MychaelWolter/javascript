8b6aac00edef3e0e08d9b5024289caf3c5bf79a5
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2025-02-03 15:58:44 -0400

    aula 46 js insert and insertBefore

--- aula/arrays/aula42/aula42.js
+++ aula/arrays/aula42/aula42.js
@@ -13,19 +13,27 @@ console.log(greatAges);
 console.log(lessAges);
 
 
+// BASSICO //
+
 
 const form = document.querySelector("#form-container");
 const formInput = document.querySelector("#input-course");
 
+const buttonAddBefore = document.querySelector("#to-add-course-before");
+const buttonAddAfter = document.querySelector("#to-add-course-after");
 const buttonAdd = document.querySelector("#to-add-course");
+
 const buttonRemove = document.querySelector("#remove-course");
 const buttonSelect = document.querySelector("#select-course");
 
 const containerResult = document.querySelector("#result-container");
+const checkboxs = [...document.querySelectorAll("input[type=checkbox]")];
 
 
+// FUNÇÕES //
 
-const createCourse = (courseInput) => {
+
+const createCourse = (courseInput, action, courseSibling) => {
     const div = document.createElement("div");
     div.classList.add("course");
 
@@ -38,14 +46,35 @@ const createCourse = (courseInput) => {
     checkbox.id = courseInput;
     div.appendChild(checkbox);
 
-    containerResult.appendChild(div);
+    if (action === "add") {
+        containerResult.appendChild(div);
+    };
+
+    if (action === "before") {
+        containerResult.insertBefore(div, courseSibling);
+    };
+
+    if (action === "after") {
+        containerResult.insertBefore(div, courseSibling.nextSibling);
+    };
+
     formInput.focus();
     formInput.value = "";
 };
 
-const check = (action) => {
-    const checkboxs = [...document.querySelectorAll("input[type=checkbox]")];
 
+
+const beforeAndAfter = () => {
+    const checkboxSelect = checkboxs.filter(checkbox => checkbox.checked);
+
+    const courseSelect = checkboxSelect[0].parentNode;
+
+    return courseSelect;
+};
+
+
+
+const check = (action) => {
     const selectCheckbos = checkboxs.filter(checkbox => checkbox.checked);
 
     const coursesSelect = selectCheckbos.map(selectCheckbox => selectCheckbox.parentNode);
@@ -64,20 +93,44 @@ const check = (action) => {
 };
 
 
+// EVENTOS //
 
-form.addEventListener("submit", (event) => {
+
+buttonAddBefore.addEventListener("click", (event) => {
     event.preventDefault();
     const formInputValue = formInput.value;
 
     if (formInputValue) {
-        createCourse(formInputValue);
+        createCourse(formInputValue, "before", beforeAndAfter());
     };
 });
 
-buttonRemove.addEventListener("click", () => {
+buttonAddAfter.addEventListener("click", (event) => {
+    event.preventDefault();
+    const formInputValue = formInput.value;
+
+    if (formInputValue) {
+        createCourse(formInputValue, "after", beforeAndAfter());
+    };
+});
+
+
+
+buttonAdd.addEventListener("click", (event) => {
+    event.preventDefault();
+    const formInputValue = formInput.value;
+
+    if (formInputValue) {
+        createCourse(formInputValue, "add");
+    };
+});
+
+buttonRemove.addEventListener("click", (event) => {
+    event.preventDefault();
     check("remove");
 });
 
-buttonSelect.addEventListener("click", () => {
+buttonSelect.addEventListener("click", (event) => {
+    event.preventDefault();
     check("select");
 });
