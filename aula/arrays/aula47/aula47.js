bf7880f5b83e896f5e89dfde33ed37e4fcd85550
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2025-02-11 15:09:06 -0400

    finalizando aula 47 sobre metoo find

--- aula/arrays/aula47/aula47.js
+++ aula/arrays/aula47/aula47.js
@@ -5,18 +5,20 @@ const result = document.querySelector("#result");
 
 const elementsArray = [9, 15, 43, 21, 5, 56, 78, 24, 16, 37, 2, 10];
 
-array.innerHTML = `[${elementsArray} ]`;
-
-const verification = () => {
-
-};
+array.innerHTML = `[${elementsArray}]`;
 
 buttonSearch.addEventListener("click", () => {
     
-    const inputSearchValue = inputSearch.value;
-    console.log(inputSearchValue)
+    const inputSearchValue = parseInt(inputSearch.value);
 
-    const elementFound = elementsArray.find(verification());
+    const elementFound = elementsArray.find((itemArray) => {
+        if (itemArray === inputSearchValue) {
+            return itemArray;
+        };
+    });
 
-    result.innerHTML = elementFound;
+    elementFound ? result.innerHTML = elementFound : result.innerHTML = `Não está presente no array`;
+
+    inputSearch.value = "";
+    inputSearch.focus();
 });
