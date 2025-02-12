40d9e5d741fdd4e961a48c65d7a13e499699f532
Mychael Lucio Wolter dos Santos <mychaelwolter1025@gmail.com>
2025-02-12 14:26:28 -0400

    finalizando aula sobre every

--- aula/arrays/aula48/aula48.js
+++ aula/arrays/aula48/aula48.js
@@ -7,12 +7,17 @@ const elementsArray = [12, 87, 45, 23, 91, 34, 76, 58, 19, 63, 82, 27, 99, 50];
 array.innerHTML = `[${elementsArray}]`;
 
 buttonToCheck.addEventListener("click", () => {
+    const arrayTrue = [];
+    const arrayFalse = [];
 
     const toCheck = elementsArray.every((itemArray) => {
         if (itemArray % 2 === 0) {
-            return itemArray;
+            arrayTrue.push(itemArray);
+        } else {
+            arrayFalse.push(itemArray);
         };
+        return itemArray;
     });
 
-    toCheck ? result.innerHTML = `Todos correspondem` : result.innerHTML = `Nem todos correspondem`;
+    toCheck ? result.innerHTML = `Todos correspondem ${arrayTrue}` : result.innerHTML = `Nem todos correspondem ${indexArray}`;
 });
